5143fc85bfb52600d722a1257efe42e09c7e8006
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2020-11-26 09:33:34 +0000

    yo did the thing

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>Daily bulletin</title>
+<style>
+td{
+	font-family:Comic Sans MS;
+	max-width: 1000px;
+	word-wrap: break-word;
+}
+.top{
+	font-weight: bold;
+	font-size:20px;
+}
+</style>
+</head>
+	<body>
+
+	<h1 style="font-size:50; width:100%; text-align:center;">Bulletin</h1>
+	<hr>
+	<table border="2" style="border-collapse: collapse; width: 80%; margin-left:10%; margin-right: 10%">
+	<tr>
+	<td class="top">Event</td>
+	<td class="top">Description</td>
+	<td class="top">Date</td>
+	</tr>
+	<tr>
+	<td>Daniels birthday</td>
+	<td>No one a;lkjads;lfkja;dskjf;alkdsjf;lkjdslflkjdslfkjsdlfkjsdlkfjlskdjflksjdflkjsldfkjsldkfjlskdjflksjdflkjsdlfkjdslfkjdlkjcareslkjdslfkjsdlfkjsdlkfjlskdjflksjdflkjsldfkjsldkfjlskdjflksjdflkjsdlfkjdslfkjdlkjcareslkjdslfkjsdlfkjsdlkfjlskdjflksjdflkjsldfkjsldkfjlskdjflksjdflkjsdlfkjdslfkjdlkjcareslkjdslfkjsdlfkjsdlkfjlskdjflksjdflkjsldfkjsldkfjlskdjflksjdflkjsdlfkjdslfkjdlkjcareskjsdlfkjsdlkfjlskdjflksjdflkjsldfkjsldkfjlskdjflksjdflkjsdlfkjdslfkjdlkjcares</td>
+	<td>IDK</td>
+	</tr>
+	<tr>
+	<td>Ultimate frizbee</td>
+	<td>Ultimate firzbee tournament</td>
+	<td>Like a week ago</td>
+	</tr>
+	</body>
+</html>
